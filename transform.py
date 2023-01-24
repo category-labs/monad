4a5b662c7921dd4fe5d3fd63b1b6768b24915371
Alexander Lee <alex@monad.xyz>
2023-01-24 15:42:24 -0500

    transform python logic

---
+++ transform.py
@@ -0,0 +1,71 @@
+from siblings import longest_common_prefix
+from nodes import Leaf, Branch
+
+# merge the two nodes together
+# either a new branch is created or branches are concatenated together
+def merge(s_2, s_3):
+    common_prefix = longest_common_prefix(s_2, s_3)
+    
+    if s_2.path == common_prefix:
+        assert(isinstance(s_2, Branch))
+        return Branch(s_2.path, s_2.branches + [s_3.path[len(common_prefix)]])
+    
+    return Branch(common_prefix, [s_2.path[len(common_prefix)],
+                                  s_3.path[len(common_prefix)]])
+
+# Returns s_2 and s_3 merged, or None if no action
+def transform(s_1, s_2, s_3, s_4):
+    assert(s_2 is not None and s_3 is not None)
+
+    if s_1 is None and s_4 is None:
+        return merge(s_2, s_3)
+    
+    curr_prefix_len = len(longest_common_prefix(s_2, s_3))
+    
+    if s_1 is None:
+        if curr_prefix_len >= len(longest_common_prefix(s_3, s_4)):
+            return merge(s_2, s_3)
+        return None
+    
+    if s_4 is None:
+        if curr_prefix_len > len(longest_common_prefix(s_1, s_2)):
+            return merge(s_2, s_3)
+        return None
+    
+    if curr_prefix_len >= len(longest_common_prefix(s_3, s_4)) and \
+        curr_prefix_len > len(longest_common_prefix(s_1, s_2)):
+        return merge(s_2, s_3)
+    
+    return None
+
+def main():
+    # merge two leaves together
+    assert(transform(None                    , Leaf("1234"), Leaf("1235"), None)         == Branch("123", ["4", "5"]))
+    assert(transform(Branch("12", ["1", "2"]), Leaf("1234"), Leaf("1235"), None)         == Branch("123", ["4", "5"]))
+    assert(transform(None                    , Leaf("1234"), Leaf("1235"), Leaf("1237")) == Branch("123", ["4", "5"]))
+    assert(transform(Branch("12", ["1", "2"]), Leaf("1234"), Leaf("1235"), Leaf("1237")) == Branch("123", ["4", "5"]))
+
+    # no action
+    assert(transform(Branch("123", ["1", "2"]) , Leaf("1234") , Leaf("1235") , None)          == None)
+    assert(transform(None                      , Leaf("12345"), Leaf("12356"), Leaf("12357")) == None)
+    assert(transform(Branch("1234", ["1", "2"]), Leaf("12345"), Leaf("12356"), Leaf("12345")) == None)
+
+    # extend a branch
+    assert(transform(None, Branch("123", ["1", "2"]), Leaf("1235"), None) == Branch("123", ["1", "2", "5"]))
+    assert(transform(None, Branch("123", ["1", "2"]), Leaf("1235"), Leaf("1236")) == Branch("123", ["1", "2", "5"]))
+    assert(transform(Branch("12", ["1", "2"]), Branch("123", ["1", "2"]), Leaf("1235"), Leaf("1236")) == Branch("123", ["1", "2", "5"]))
+    assert(transform(Branch("12", ["1", "2"]), Branch("123", ["1", "2"]), Branch("1235", ["1", "2"]), Leaf("1236")) == Branch("123", ["1", "2", "5"]))
+
+    # emitting new branch
+    assert(transform(None, Branch("123", ["1", "2"]), Branch("124", ["1", "2"]), None) == Branch("12", ["3", "4"]))
+    assert(transform(Branch("1", ["1", "2"]), Branch("123", ["1", "2"]), Branch("124", ["1", "2"]), None) == Branch("12", ["3", "4"]))
+    assert(transform(None, Branch("123", ["1", "2"]), Branch("124", ["1", "2"]), Leaf("1256")) == Branch("12", ["3", "4"]))
+    assert(transform(Branch("1", ["1", "2"]), Branch("123", ["1", "2"]), Branch("124", ["1", "2"]), Leaf("1256")) == Branch("12", ["3", "4"]))
+
+    assert(transform(None, Branch("123", ["1", "2"]), Leaf("1245"), None) == Branch("12", ["3", "4"]))
+    assert(transform(Leaf("1567"), Branch("123", ["1", "2"]), Leaf("1245"), None) == Branch("12", ["3", "4"]))
+    assert(transform(None, Branch("123", ["1", "2"]), Leaf("1245"), Leaf("1268")) == Branch("12", ["3", "4"]))
+    assert(transform(Leaf("1567"), Branch("123", ["1", "2"]), Leaf("1245"), Leaf("1268")) == Branch("12", ["3", "4"]))
+
+if __name__ == "__main__":
+    main()

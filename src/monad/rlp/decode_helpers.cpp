660041ebb63f4fac20728453f1f75cef465bd003
Gabriel Lee <glee@peach4.spacetime.org>
2023-02-09 16:02:42 -0500

    Added untested decode_acount without decode_access_list

--- src/monad/rlp/decode_helpers.cpp
+++ src/monad/rlp/decode_helpers.cpp
@@ -1,6 +1,7 @@
 #include <monad/rlp/decode_helpers.hpp>
 
 #include <monad/core/account.hpp>
+#include <monad/core/address.hpp>
 #include <monad/core/assert.h>
 #include <monad/core/receipt.hpp>
 #include <monad/core/signature.hpp>
@@ -12,8 +13,14 @@
 MONAD_RLP_NAMESPACE_BEGIN
 
 template <typename T>
-inline void decode_to_field_and_update_ptr(byte_string_view const enc, T &field, byte_string_loc &i) {
+inline void decode_unsigned_to_field_and_update_ptr(byte_string_view const enc, T &field, byte_string_loc &i) {
     auto dec_with_ptr = decode_unsigned(enc, i);
+    field = dec_with_ptr.decoding;
+    i = dec_with_ptr.ptr;
+}
+
+inline void decode_string_to_field_and_update_ptr(byte_string_view const enc, byte_string &field, byte_string_loc &i) {
+    auto dec_with_ptr = decode_string(enc, i);
     field = dec_with_ptr.decoding;
     i = dec_with_ptr.ptr;
 }
@@ -23,6 +30,20 @@ inline void decode_bytes32_to_field_and_update_ptr(byte_string_view const enc, b
     MONAD_ASSERT(dec_with_ptr.decoding.size() == 32);
     // glee for shea: is memcpy appropriate?
     memcpy(field.bytes, dec_with_ptr.decoding.data(), 32);
+    i = dec_with_ptr.ptr;
+}
+
+inline void decode_address_to_field_and_update_ptr(byte_string_view const enc, address_t &field, byte_string_loc &i) {
+    auto dec_with_ptr = decode_string(enc, i);
+    MONAD_ASSERT(dec_with_ptr.decoding.size() == 20);
+    // glee for shea: is memcpy appropriate?
+    memcpy(field.bytes, dec_with_ptr.decoding.data(), 20);
+    i = dec_with_ptr.ptr;
+}
+
+inline void decode_sc_to_field_and_update_ptr(byte_string_view const enc, SignatureAndChain &sc, byte_string_loc &i) {
+    auto dec_with_ptr = decode_unsigned(enc, i);
+    from_v(sc, dec_with_ptr.decoding);
     i = dec_with_ptr.ptr;
 }
 
@@ -55,8 +76,8 @@ std::pair<Account, bytes32_t> decode_account(byte_string_view const enc)
     }
     MONAD_ASSERT(i + length == enc.size());
 
-    decode_to_field_and_update_ptr(enc, acc.nonce, i);
-    decode_to_field_and_update_ptr(enc, acc.balance, i);
+    decode_unsigned_to_field_and_update_ptr(enc, acc.nonce, i);
+    decode_unsigned_to_field_and_update_ptr(enc, acc.balance, i);
     decode_bytes32_to_field_and_update_ptr(enc, code_root, i);
     decode_bytes32_to_field_and_update_ptr(enc, acc.code_hash, i);
 
@@ -64,7 +85,6 @@ std::pair<Account, bytes32_t> decode_account(byte_string_view const enc)
     return std::make_pair(acc, code_root);
 }
 
-/*
 decoding_with_updated_ptr<Transaction> decode_transaction(byte_string_view const enc, byte_string_loc i)
 {
     MONAD_ASSERT(i < enc.size());
@@ -110,45 +130,47 @@ decoding_with_updated_ptr<Transaction> decode_transaction(byte_string_view const
 
     if (txn.type == Transaction::Type::eip155)
     {
-        {
-            auto dec_with_ptr = decode_unsigned(enc, i);
-            txn.nonce = dec_with_ptr.decoding;
-            i = dec_with_ptr.ptr;
-        }
-
-        {
-            auto dec_with_ptr = decode_unsigned(enc, i);
-            acc.balance = dec_with_ptr.decoding;
-            i = dec_with_ptr.ptr;
-        }
-
-        // glee for shea: is memcpy appropriate?
-        {
-            auto dec_with_ptr = decode_string(enc, i);
-            MONAD_ASSERT(dec_with_ptr.decoding.size() == 32);
-            memcpy(code_root.bytes, dec_with_ptr.decoding.data(), 32);
-            i = dec_with_ptr.ptr;
-        }
-
-        {
-            auto dec_with_ptr = decode_string(enc, i);
-            MONAD_ASSERT(dec_with_ptr.decoding.size() == 32);
-            memcpy(acc.code_hash.bytes, dec_with_ptr.decoding.data(), 32);
-            i = dec_with_ptr.ptr;
-        }
+        decode_unsigned_to_field_and_update_ptr(enc, txn.nonce, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.gas_price, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.gas_limit, i);
+        decode_address_to_field_and_update_ptr(enc, *txn.to, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.amount, i);
+        decode_string_to_field_and_update_ptr(enc, txn.data, i);
+        decode_sc_to_field_and_update_ptr(enc, txn.sc, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.sc.r, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.sc.s, i);
     }
     else if (txn.type == Transaction::Type::eip1559)
     {
-
+        decode_unsigned_to_field_and_update_ptr(enc, *txn.sc.chain_id, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.nonce, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.priority_fee, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.gas_price, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.gas_limit, i);
+        decode_address_to_field_and_update_ptr(enc, *txn.to, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.amount, i);
+        decode_string_to_field_and_update_ptr(enc, txn.data, i);
+        // TODO access list
+        decode_unsigned_to_field_and_update_ptr(enc, txn.sc.odd_y_parity, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.sc.r, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.sc.s, i);
     }
     else            // Transaction::type::eip2930
     {
-
+        decode_unsigned_to_field_and_update_ptr(enc, *txn.sc.chain_id, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.nonce, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.gas_price, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.gas_limit, i);
+        decode_address_to_field_and_update_ptr(enc, *txn.to, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.amount, i);
+        decode_string_to_field_and_update_ptr(enc, txn.data, i);
+        // TODO access list
+        decode_unsigned_to_field_and_update_ptr(enc, txn.sc.odd_y_parity, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.sc.r, i);
+        decode_unsigned_to_field_and_update_ptr(enc, txn.sc.s, i);
     }
 
-    MONAD_ASSERT(i == enc.size());
-    return std::make_pair(acc, code_root);
+    return {txn, i};
 }
-*/
 
 MONAD_RLP_NAMESPACE_END

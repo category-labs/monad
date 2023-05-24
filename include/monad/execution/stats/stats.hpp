637bb2cb33110633315b3f5926360633a068e712
Tong Zhi <tzhi@monad.xyz>
2023-05-24 10:46:43 -0400

    :new: Add Stats Class
    Problem:
    1. We need something to log our execution data for debugging & tracing purposes
    
    Solution:
    1. As opposed to normal logging, which lacks context (i.e. let's say we log a transaction, it's gonna be hard for us to figure out which block it belongs to), this Stats class solves the problem by putting everything into context

---
+++ include/monad/execution/stats/stats.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <monad/execution/stats/config.hpp>
+
+#include <monad/core/account.hpp>
+#include <monad/core/block.hpp>
+#include <monad/core/byte_string.hpp>
+#include <monad/core/transaction.hpp>
+
+#include <chrono>
+#include <unordered_map>
+#include <vector>
+
+MONAD_EXECUTION_STATS_NAMESPACE_BEGIN
+
+/*
+Currently only doing things at txn level
+State level details will be added later
+*/
+
+struct TxnStats
+{
+    Transaction txn_;
+
+    TxnStats(Transaction t, size_t id) noexcept
+        : txn_{t}
+        , id_{id}
+    {
+    }
+
+    size_t id_;
+    int status_; // not used currently
+    std::chrono::_V2::steady_clock::time_point start_time_;
+    std::chrono::_V2::steady_clock::time_point finished_time_;
+    uint64_t elapsed_ms_;
+
+    // txn might fail due to optimistic execution
+    // so we include all the tries, should include info like states touched
+    // but since state is not fully ready yet, just this interface for now
+    struct Tries
+    {
+        std::chrono::_V2::steady_clock::time_point start_time_;
+        std::chrono::_V2::steady_clock::time_point stopped_time_;
+        uint64_t elapsed_ms_;
+
+        int status_; // not used currently
+    };
+
+    std::vector<Tries> tries_;
+};
+
+struct BlockStats
+{
+    Block block_;
+
+    explicit BlockStats(Block b) noexcept
+        : block_{b}
+    {
+    }
+
+    std::vector<TxnStats> finished_txns_;
+    std::vector<TxnStats> running_txns_;
+    std::unordered_map<size_t, size_t> running_txns_map_;
+
+    std::chrono::_V2::steady_clock::time_point start_time_;
+    std::chrono::_V2::steady_clock::time_point finished_time_;
+    uint64_t elapsed_ms_;
+
+    struct Snapshot
+    {
+        std::chrono::_V2::steady_clock::time_point time_;
+        uint64_t time_interval_ms_;
+        std::vector<TxnStats> finished_txns_;
+        std::vector<TxnStats> running_txns_;
+    };
+
+    std::vector<Snapshot> snapshots_;
+};
+
+MONAD_EXECUTION_STATS_NAMESPACE_END

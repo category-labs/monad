7efd80409d830e58daffaab2683de43f20ab59f7
Rene Garcia <rgarc@alum.mit.edu>
2023-06-15 11:39:45 -0400

    :white_check_mark: Add test cases for modular exponentiation.
    Problem:
    - We don't have a basic test suite for the modular exponentiation precompile.
    - In the previous commit, we refactored the Byzantium precompile list to use type list operations.
    
    Solution:
    - Port a test suite for the modular exponentiation precompile from go-ethereum into our repo.
    - Add a compile time check to assert that the type list we ended up with in the previous commit is the same as if we had just written all the precompiles in a type list.

--- include/monad/execution/ethereum/static_precompiles/modular_exponentiation.hpp
+++ include/monad/execution/ethereum/static_precompiles/modular_exponentiation.hpp
@@ -10,7 +10,11 @@ namespace static_precompiles
     template <class TFork>
     struct ModularExponentiation
     {
-        static evmc::Result execute(const evmc_message &m) noexcept;
+        static evmc::Result execute(const evmc_message &message) noexcept
+        {
+            (void)message;
+            return evmc::Result{evmc_result{}};
+        }
     };
 }
 

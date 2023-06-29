8edd0623db80d3ed95d92f2956545e7cd806aea7
Rene Garcia <rgarc@alum.mit.edu>
2023-06-29 14:27:24 -0400

    :new: Add command-line utility to parse ethereum tests and execute transactions using monad code.
    Problem:
    - Previously, a hand-written list `all_forks_t` was being used as a type
      list for all the forks which is slightly repetitive.
    - The CLI helper string that displayed the list of valid fork names was
      itself repetitive.
    - Previously,deciding which fork to execute at runtime relied on a
      hand-rolled switch statement which was tedious.
    - Failing to validate a transaction before executing will result in a
      gas usage mismatch.
    - We previously set the nonce of the miner's wallet to 1 to prevent
      account deletion.
    - Previously, the block reward was not being applied to the miner's
      balance.
    
    Solution:
    - Traverse all the forks at compile time by using the `next_fork_t`
      member type alias.
    - Compute the string at runtime by demangling each fork name and taking
      the last token of splitting by ::.
    - Use mp_for_each to dispatch into the correct fork for brevity.
    - Validate transaction before executing, leaving the failure path as a
      TODO for now.
    - Stop setting the nonce of the miner to 1.
    - Apply the block award to the miner after executing transactions.

---
+++ test/compatibility/compatibility.cpp
@@ -0,0 +1,401 @@
+#include "from_json.hpp"
+#include <CLI/CLI.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/core/demangle.hpp>
+#include <boost/mp11.hpp>
+#include <evmc/evmc.hpp>
+#include <format>
+#include <iostream>
+#include <monad/core/account.hpp>
+#include <monad/db/in_memory_trie_db.hpp>
+#include <monad/execution/ethereum/fork_traits.hpp>
+#include <monad/execution/ethereum/genesis.hpp>
+#include <monad/execution/evm.hpp>
+#include <monad/execution/evmc_host.hpp>
+#include <monad/execution/evmone_baseline_interpreter.hpp>
+#include <monad/execution/static_precompiles.hpp>
+#include <monad/execution/test/fakes.hpp>
+#include <monad/execution/transaction_processor.hpp>
+#include <nlohmann/json.hpp>
+#include <ranges>
+#include <string>
+#include <tl/expected.hpp>
+#include <unordered_map>
+#include <variant>
+
+#include <monad/state/account_state.hpp>
+#include <monad/state/code_state.hpp>
+#include <monad/state/state.hpp>
+#include <monad/state/value_state.hpp>
+
+namespace fs = std::filesystem;
+
+struct FileDoesNotExist
+{
+};
+
+struct JSONParseError
+{
+};
+
+using Error = std::variant<FileDoesNotExist, JSONParseError>;
+
+tl::expected<std::string, Error> read_file(fs::path const &file_path)
+{
+    if (!fs::exists(file_path)) {
+        return tl::make_unexpected(Error{FileDoesNotExist{}});
+    }
+    std::ifstream file{file_path};
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
+std::optional<size_t> to_fork_index(std::string_view s)
+{
+    std::string copy{s.data(), s.size()};
+    std::transform(s.begin(), s.end(), copy.begin(), [](auto c) {
+        return std::tolower(c);
+    });
+    if (s == "frontier") {
+        return 0;
+    }
+    if (s == "homestead") {
+        return 1;
+    }
+    if (s == "spurious_dragon") {
+        return 2;
+    }
+    if (s == "byzantium") {
+        return 3;
+    }
+    if (s == "constantinople") {
+        return 4;
+    }
+    if (s == "istanbul") {
+        return 5;
+    }
+    if (s == "berlin") {
+        return 6;
+    }
+    if (s == "london") {
+        return 7;
+    }
+    return std::nullopt;
+}
+
+using account_store_db_t = monad::db::InMemoryTrieDB;
+using code_db_t = std::unordered_map<monad::address_t, monad::byte_string>;
+using state_t = monad::state::State<
+    monad::state::AccountState<account_store_db_t>,
+    monad::state::ValueState<account_store_db_t>,
+    monad::state::CodeState<code_db_t>, monad::db::BlockDb, account_store_db_t>;
+using working_state_t = decltype(std::declval<state_t>().get_new_changeset(0u));
+
+template <typename TFork>
+using interpreter_t =
+    monad::execution::EVMOneBaselineInterpreter<working_state_t, TFork>;
+
+template <typename TFork>
+using transaction_processor_t =
+    monad::execution::TransactionProcessor<working_state_t, TFork>;
+
+template <typename TFork>
+using static_precompiles_t = monad::execution::StaticPrecompiles<
+    working_state_t, TFork, typename TFork::static_precompiles_t>;
+
+template <typename TFork>
+using evm_t = monad::execution::Evm<
+    working_state_t, TFork, static_precompiles_t<TFork>, interpreter_t<TFork>>;
+
+template <typename TFork>
+using host_t = monad::execution::EvmcHost<working_state_t, TFork, evm_t<TFork>>;
+
+using namespace boost::mp11;
+using namespace monad::fork_traits;
+
+template <typename T>
+constexpr auto Traverse()
+{
+    if constexpr (requires { typename T::next_fork_t; }) {
+        return boost::mp11::
+            mp_push_front<decltype(Traverse<typename T::next_fork_t>()), T>{};
+    }
+    else {
+        return boost::mp11::mp_list<T>{};
+    }
+}
+
+template <>
+constexpr auto Traverse<no_next_fork_t>()
+{
+    return boost::mp11::mp_list<no_next_fork_t>{};
+}
+
+using all_forks_t = decltype(Traverse<monad::fork_traits::frontier>());
+static_assert(mp_size<all_forks_t>::value == 8);
+
+template <typename TFork>
+struct Execution
+{
+    Execution(
+        monad::BlockHeader const &block_header,
+        monad::Transaction const &transaction, working_state_t &state)
+        : host_{block_header, transaction, state}
+        , transaction_processor_{}
+    {
+    }
+
+    monad::Receipt
+    execute(working_state_t &state, monad::Transaction const &transaction)
+    {
+        using Status = typename transaction_processor_t<TFork>::Status;
+        typename decltype(transaction_processor_)::Status status =
+            transaction_processor_.validate(
+                state,
+                transaction,
+                host_.block_header_.base_fee_per_gas.value_or(0));
+        if (status != Status::SUCCESS) {
+            throw std::runtime_error("properly handle invalid transactions");
+        }
+        return transaction_processor_.execute(
+            state,
+            host_,
+            transaction,
+            host_.block_header_.base_fee_per_gas.value_or(0));
+    }
+
+    host_t<TFork> host_;
+    transaction_processor_t<TFork> transaction_processor_;
+};
+
+// creates a sum type over all the forks:
+// std::variant<std::monostate, Execution<frontier>, Execution<homestead>, ...>
+using execution_variant =
+    decltype([]<typename... Types>(boost::mp11::mp_list<Types...>) {
+        return std::variant<std::monostate, Types...>{};
+    }(mp_transform<Execution, all_forks_t>{}));
+
+/**
+ * execute a transaction with a given state using a fork specified at runtime
+ * @param fork_index
+ * @param state
+ * @param transaction
+ * @return a receipt of the transaction
+ */
+monad::Receipt execute(
+    size_t fork_index, monad::BlockHeader const &block_header,
+    working_state_t &state, monad::Transaction const &transaction)
+{
+    // makes a glorified jump table by creating an array where the i'th index
+    // corresponds to an instance of the Execution struct with the i'th fork
+    // injected.
+    auto execution_array = []<typename... Ts>(
+                               mp_list<Ts...>,
+                               monad::BlockHeader const &block_header,
+                               working_state_t &state,
+                               monad::Transaction const &transaction) {
+        return std::array<execution_variant, sizeof...(Ts)>{
+            Execution<Ts>{block_header, transaction, state}...};
+    }(all_forks_t{}, block_header, state, transaction);
+
+    // we then dispatch into the appropriate fork at runtime using std::get
+    auto &variant = execution_array.at(fork_index);
+
+    std::optional<monad::Receipt> maybe_receipt;
+    mp_for_each<mp_iota_c<mp_size<all_forks_t>::value>>([&](auto I) {
+        if (I == fork_index) {
+            maybe_receipt =
+                std::get<Execution<mp_at_c<all_forks_t, I>>>(variant).execute(
+                    state, transaction);
+        }
+    });
+
+    return maybe_receipt.value();
+}
+
+/**
+ * @param path
+ * @return if `path` is a directory, return a list of paths that is a result of
+ * recursively walking the directory, or a list with the single path if `path`
+ * is a file.
+ */
+std::vector<fs::path> walk(fs::path const &path)
+{
+    if (is_directory(path)) {
+        std::vector<fs::path> res;
+        for (auto const &p : fs::recursive_directory_iterator(path)) {
+            res.push_back(p);
+        }
+        return res;
+    }
+    return {path};
+}
+
+int main(int argc, char **argv)
+{
+    [[maybe_unused]] auto *compatibility_logger =
+        monad::log::logger_t::create_logger("compatibility_logger");
+    [[maybe_unused]] auto *trie_db_logger =
+        monad::log::logger_t::create_logger("trie_db_logger");
+    [[maybe_unused]] auto *change_set_logger =
+        monad::log::logger_t::create_logger("change_set_logger");
+    change_set_logger->set_log_level(quill::LogLevel::Debug);
+    [[maybe_unused]] auto *evmone_baseline_interpreter_logger =
+        monad::log::logger_t::create_logger(
+            "evmone_baseline_interpreter_logger");
+    evmone_baseline_interpreter_logger->set_log_level(quill::LogLevel::Debug);
+
+    monad::log::logger_t::start();
+
+    MONAD_LOG_INFO(
+        compatibility_logger,
+        "running from working directory {}",
+        fs::current_path());
+
+    CLI::App cli{"compatibility"};
+
+    std::filesystem::path test_suite;
+    cli.add_option(
+           "-t, --test_suite",
+           test_suite,
+           "either a path to a directory containing a test suite in the "
+           "https://github.com/ethereum/tests.git repository, or a path to "
+           "single .json test")
+        ->required();
+
+    std::string fork_name;
+    cli.add_option(
+           "-f, --fork", fork_name, "name of the ethereum fork to run on")
+        ->required()
+        ->check([](auto const &fork_name) -> std::string {
+            auto const index = to_fork_index(fork_name);
+            if (index.has_value()) {
+                return "";
+            }
+            else {
+                return std::format(
+                    "invalid fork name, must be one of: {}", []() {
+                        std::vector<std::string> fork_names;
+                        mp_for_each<all_forks_t>([&](auto fork) {
+                            std::vector<std::string> tokens;
+                            auto const demangled_name =
+                                boost::core::demangle(typeid(fork).name());
+                            boost::algorithm::split(
+                                tokens, demangled_name, boost::is_any_of("::"));
+                            if (!tokens.empty()) {
+                                fork_names.push_back(tokens.back());
+                            }
+                        });
+                        return boost::algorithm::join(fork_names, ", ");
+                    }());
+            }
+        });
+
+    cli.parse(argc, argv);
+
+    auto const fork_index = to_fork_index(fork_name).value();
+
+    for (auto const &directory_entry : walk(test_suite)) {
+        if (directory_entry.extension() != ".json") {
+            continue;
+        }
+
+        auto const maybe_file_contents = read_file(directory_entry);
+        MONAD_LOG_INFO(
+            compatibility_logger, "running tests from {}", directory_entry);
+
+        if (!maybe_file_contents) {
+            std::cerr << "failed to read file: " << directory_entry
+                      << std::endl;
+            continue;
+        }
+        auto const json = nlohmann::json::parse(
+            maybe_file_contents.value())[directory_entry.stem()];
+
+        if (!json.contains("pre")) {
+            continue;
+        }
+
+        monad::db::BlockDb blocks{"some_path"};
+        account_store_db_t db{};
+        monad::state::AccountState accounts{db};
+        monad::state::ValueState values{db};
+        code_db_t code_db{};
+        monad::state::CodeState codes{code_db};
+        monad::state::State s{accounts, values, codes, blocks, db};
+        MONAD_LOG_INFO(
+            compatibility_logger,
+            "root hash before transactions: {}",
+            evmc::hex(db.root_hash()));
+        quill::flush();
+
+        auto working_state = s.get_new_changeset(0u);
+
+        auto block_header =
+            ::test::beneficiary_from_json(working_state, json["env"]);
+        test::from_json(working_state, json["pre"]);
+
+        auto const transactions =
+            ::test::from_json<std::vector<monad::Transaction>>(
+                json["transaction"]);
+
+        size_t transaction_index = 0;
+        uint64_t total_gas_used = 0;
+        for (auto const &transaction : transactions) {
+            MONAD_LOG_INFO(
+                compatibility_logger,
+                "execution transaction {}",
+                transaction_index);
+            auto receipt =
+                execute(fork_index, block_header, working_state, transaction);
+
+            MONAD_LOG_INFO(
+                compatibility_logger,
+                "gas used during transaction {} = {}",
+                transaction_index,
+                intx::to_string(monad::uint256_t{receipt.gas_used}, 16));
+
+            total_gas_used += receipt.gas_used;
+
+            assert(receipt.status);
+            transaction_index++;
+        }
+
+        MONAD_LOG_INFO(compatibility_logger, "merging and committing");
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(5s);
+
+        std::vector<monad::address_t> changed_account_addresses;
+        std::transform(
+            working_state.accounts_.changed_.begin(),
+            working_state.accounts_.changed_.end(),
+            std::back_inserter(changed_account_addresses),
+            [](const auto &change) { return change.first; });
+        s.merge_changes(working_state);
+        s.apply_block_reward(
+            block_header.beneficiary,
+            monad::uint256_t{0} /* make block reward configurable */);
+        s.commit();
+        auto const post_hash = evmc::hex(db.root_hash());
+        MONAD_LOG_INFO(
+            compatibility_logger,
+            "root hash after transactions: {}",
+            post_hash);
+        MONAD_LOG_INFO(
+            compatibility_logger,
+            "account state hash after transactions: {}",
+            evmc::hex(s.get_state_hash()));
+        MONAD_LOG_INFO(
+            compatibility_logger,
+            "total gas used: {}",
+            intx::to_string(monad::uint256_t{total_gas_used}, 16));
+        auto const post_state = test::to_json(s, changed_account_addresses);
+        MONAD_LOG_INFO(
+            compatibility_logger, "post state: {}", post_state.dump(2));
+
+        quill::flush();
+    }
+    quill::remove_logger(trie_db_logger);
+    return 0;
+}
---
+++ test/compatibility/from_json.hpp
@@ -0,0 +1,202 @@
+#pragma once
+
+#include <ethash/keccak.hpp>
+#include <monad/core/account.hpp>
+#include <monad/core/address.hpp>
+#include <monad/core/transaction.hpp>
+#include <monad/db/block_db.hpp>
+#include <monad/execution/test/fakes.hpp>
+#include <monad/logging/config.hpp>
+#include <monad/logging/monad_log.hpp>
+#include <nlohmann/json.hpp>
+#include <string>
+#include <test_resource_data.h>
+
+namespace test
+{
+    using namespace evmc::literals;
+
+    template <typename T>
+    T from_json(nlohmann::json const &json) = delete;
+
+    template <>
+    monad::address_t from_json(nlohmann::json const &json)
+    {
+        return evmc::from_hex<monad::address_t>(json.get<std::string>())
+            .value();
+    }
+
+    template <>
+    monad::uint256_t from_json(nlohmann::json const &json)
+    {
+        return intx::from_string<monad::uint256_t>(json.get<std::string>());
+    }
+
+    template <>
+    monad::uint128_t from_json(nlohmann::json const &json)
+    {
+        return intx::from_string<monad::uint128_t>(json.get<std::string>());
+    }
+
+    template <>
+    monad::byte_string from_json(nlohmann::json const &json)
+    {
+        return evmc::from_hex(json.get<std::string>()).value();
+    }
+
+    template <typename TState>
+    void from_json(TState &state, nlohmann::json const &json)
+    {
+        for (auto const &[address_string, account_json] : json.items()) {
+            auto const account_address =
+                from_json<monad::address_t>(address_string);
+            auto const balance =
+                from_json<monad::uint256_t>(account_json["balance"]);
+            auto const nonce = static_cast<uint64_t>(
+                from_json<monad::uint256_t>(account_json["nonce"]));
+
+            state.create_account(account_address);
+
+            if (account_json.contains("code")) {
+                auto const code = from_json<monad::byte_string>(
+                    account_json["code"].get<std::string>());
+                state.set_code(account_address, code);
+            }
+
+            state.set_balance(account_address, balance);
+            state.set_nonce(account_address, nonce);
+
+            // TODO: parse the storage field
+            if (json.contains("storage") && json["storage"].is_object() &&
+                !json["storage"].empty()) {
+                throw std::runtime_error(
+                    "you forgot to parse the storage field on an object that "
+                    "has non-zero storage");
+            }
+        }
+    }
+
+    std::string hex0x(const monad::address_t &a)
+    {
+        return "0x" + evmc::hex(a);
+    }
+
+    std::string hex0x(const monad::uint256_t &n)
+    {
+        return "0x" + intx::hex(n);
+    }
+
+    std::string hex0x(const monad::byte_string_view &n)
+    {
+        return "0x" + evmc::hex(n);
+    }
+
+    template <typename TState>
+    nlohmann::json to_json(
+        TState const &state,
+        std::vector<monad::address_t> const &account_addresses)
+    {
+        auto res = nlohmann::json::object();
+
+        for (auto const &account_address : account_addresses) {
+            std::optional<monad::Account> const maybe_account =
+                state.accounts_.db_.try_find(account_address);
+            if (!maybe_account) {
+                continue;
+            }
+            auto const &account = maybe_account.value();
+            auto const &account_address_hex = hex0x(account_address);
+
+            res[account_address_hex] = nlohmann::json::object();
+
+            auto const code_string =
+                hex0x(state.code_.code_at(account_address));
+            if (code_string != "0x") {
+                res[account_address_hex]["code"] = code_string;
+            }
+
+            res[account_address_hex]["balance"] = hex0x(account.balance);
+
+            auto const nonce_string = hex0x(monad::uint256_t{account.nonce});
+            if (nonce_string != "0x0") {
+                res[account_address_hex]["nonce"] = nonce_string;
+            }
+        }
+
+        return res;
+    }
+
+    template <>
+    std::vector<monad::Transaction> from_json(nlohmann::json const &json)
+    {
+        [[maybe_unused]] auto const sender =
+            from_json<monad::address_t>(json["sender"].get<std::string>());
+        [[maybe_unused]] auto const to =
+            from_json<monad::address_t>(json["to"].get<std::string>());
+        auto const nonce =
+            static_cast<uint64_t>(from_json<monad::uint256_t>(json["nonce"]));
+
+        // TODO: kicking the transaction_type can down the road
+
+        [[maybe_unused]] monad::Transaction::Type transaction_type;
+
+        std::vector<monad::Transaction> res;
+
+        size_t index = 0;
+        for (auto const &data : json["data"]) {
+            auto const transaction_data = from_json<monad::byte_string>(data);
+            res.emplace_back(monad::Transaction{
+                .nonce = nonce,
+                .gas_price = static_cast<uint64_t>(
+                    from_json<monad::uint256_t>(json["gasPrice"])),
+                .gas_limit = static_cast<uint64_t>(
+                    from_json<monad::uint256_t>(json["gasLimit"][0])),
+                .amount = from_json<monad::uint128_t>(json["value"][index++]),
+                .to = to,
+                .from = sender,
+                .data = transaction_data,
+            });
+        }
+
+        return res;
+    }
+
+    template <class TState>
+    monad::BlockHeader
+    beneficiary_from_json(TState &s, nlohmann::json const &json)
+    {
+        auto const current_coinbase =
+            from_json<monad::address_t>(json["currentCoinbase"]);
+        auto const parent_hash_string =
+            from_json<monad::byte_string>(json["previousHash"]);
+        auto const parent_hash = *reinterpret_cast<monad::bytes32_t const *>(
+            parent_hash_string.data());
+        auto const difficulty =
+            from_json<monad::uint256_t>(json["currentDifficulty"]);
+
+        auto const number = static_cast<uint64_t>(
+            from_json<monad::uint256_t>(json["currentNumber"]));
+
+        auto const gas_limit = static_cast<uint64_t>(
+            from_json<monad::uint256_t>(json["currentGasLimit"]));
+        auto const timestamp = static_cast<uint64_t>(
+            from_json<monad::uint256_t>(json["currentTimestamp"]));
+
+        std::optional<uint64_t> maybe_base_fee_per_gas{std::nullopt};
+        if (json.contains("currentBaseFee")) {
+            maybe_base_fee_per_gas = static_cast<uint64_t>(
+                from_json<monad::uint256_t>(json["currentBaseFee"]));
+        }
+
+        s.create_account(current_coinbase);
+
+        return {
+            .parent_hash = parent_hash,
+            .difficulty = difficulty,
+            .number = number,
+            .gas_limit = gas_limit,
+            .timestamp = timestamp,
+            .beneficiary = current_coinbase,
+            .base_fee_per_gas = maybe_base_fee_per_gas.value_or(0u)};
+    }
+}

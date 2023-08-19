48d53560a3907a1f56ecb070c13f043a0a89d3e7
Tong Zhi <tzhi@monad.xyz>
2023-08-19 14:22:53 -0400

    :recycle: :bug: Separate accessed accounts from changed accounts
    Problem:
    - According to EIP-2929, we should only access certain accounts (from, to, access_list). However, our current design requires us to access account elsewhere to capture its original value
    
    Solution:
    - Create a separate value to keep track of accessed accounts
    - Change get and set logic to correctly capture original&updated value for our optimisitc execution

--- include/monad/state/account_state.hpp
+++ include/monad/state/account_state.hpp
@@ -4,6 +4,7 @@
 #include <monad/core/account.hpp>
 #include <monad/core/address.hpp>
 #include <monad/core/bytes.hpp>
+#include <monad/core/likely.h>
 #include <monad/core/receipt.hpp>
 
 #include <monad/state/config.hpp>
@@ -13,6 +14,7 @@
 #include <algorithm>
 #include <cassert>
 #include <unordered_map>
+#include <unordered_set>
 
 MONAD_STATE_NAMESPACE_BEGIN
 
@@ -143,8 +145,9 @@ struct AccountState
 template <typename TAccountDB>
 struct AccountState<TAccountDB>::ChangeSet : public AccountState<TAccountDB>
 {
-    change_set_t changed_{};
+    mutable change_set_t changed_{};
     uint64_t total_selfdestructs_{};
+    std::unordered_set<address_t> accessed_{};
 
     // EVMC Host Interface
     [[nodiscard]] bool account_exists(address_t const &a) const noexcept
@@ -168,53 +171,87 @@ struct AccountState<TAccountDB>::ChangeSet : public AccountState<TAccountDB>
     // EVMC Host Interface
     evmc_access_status access_account(address_t const &a)
     {
-        if (changed_.contains(a)) {
-            return EVMC_ACCESS_WARM;
+        if (!changed_.contains(a)) {
+            if (AccountState::account_exists(a)) {
+                auto const original = get_committed_storage(a);
+                changed_.emplace(a, diff_t{original, original});
+            }
+            else {
+                changed_.emplace(a, diff_t{std::nullopt, std::nullopt});
+            }
         }
-        auto const account = get_committed_storage(a).has_value()
-                                 ? get_committed_storage(a)
-                                 : Account{};
-        changed_.emplace(a, diff_t{get_committed_storage(a), account});
-        return EVMC_ACCESS_COLD;
+        auto const [_, inserted] = accessed_.insert(a);
+        if (inserted) {
+            return EVMC_ACCESS_COLD;
+        }
+        return EVMC_ACCESS_WARM;
+    }
+
+    [[nodiscard]] std::optional<Account>
+    get_account(address_t const &address) const noexcept
+    {
+        if (MONAD_LIKELY(changed_.contains(address))) {
+            return changed_.at(address).updated;
+        }
+        if (AccountState::account_exists(address)) {
+            auto const original = get_committed_storage(address);
+            changed_.emplace(address, diff_t{original, original});
+            return original;
+        }
+        changed_.emplace(address, diff_t{std::nullopt, std::nullopt});
+        return std::nullopt;
     }
 
     // EVMC Host Interface
-    [[nodiscard]] bytes32_t get_balance(address_t const &a) const noexcept
+    [[nodiscard]] bytes32_t get_balance(address_t const &address) const noexcept
     {
         return intx::be::store<bytes32_t>(
-            changed_.at(a).updated.value_or(Account{}).balance);
+            get_account(address).value_or(Account{}).balance);
     }
 
     void set_balance(address_t const &address, uint256_t new_balance) noexcept
     {
-        changed_.at(address).updated.value().balance = new_balance;
+        MONAD_DEBUG_ASSERT(changed_.contains(address));
+        if (changed_.at(address).updated.has_value()) {
+            changed_.at(address).updated.value().balance = new_balance;
+        }
+        else {
+            changed_.at(address).updated = Account{.balance = new_balance};
+        }
     }
 
     [[nodiscard]] uint64_t get_nonce(address_t const &address) const noexcept
     {
-        return changed_.at(address).updated.value_or(Account{}).nonce;
+        return get_account(address).value_or(Account{}).nonce;
     }
 
     void set_nonce(address_t const &address, uint64_t nonce) noexcept
     {
-        changed_.at(address).updated.value().nonce = nonce;
+        MONAD_DEBUG_ASSERT(changed_.contains(address));
+        if (changed_.at(address).updated.has_value()) {
+            changed_.at(address).updated.value().nonce = nonce;
+        }
+        else {
+            changed_.at(address).updated = Account{.nonce = nonce};
+        }
     }
 
     // EVMC Host Interface
     [[nodiscard]] bytes32_t
     get_code_hash(address_t const &address) const noexcept
     {
-        if (changed_.contains(address)) {
-            return changed_.at(address).updated.value_or(Account{}).code_hash;
-        }
-
-        return AccountState::get_code_hash(address);
+        return get_account(address).value_or(Account{}).code_hash;
     }
 
     void set_code_hash(address_t const &address, bytes32_t const &b) noexcept
     {
-        MONAD_DEBUG_ASSERT(changed_.at(address).updated.has_value());
-        changed_.at(address).updated.value().code_hash = b;
+        MONAD_DEBUG_ASSERT(changed_.contains(address));
+        if (changed_.at(address).updated.has_value()) {
+            changed_.at(address).updated.value().code_hash = b;
+        }
+        else {
+            changed_.at(address).updated = Account{.code_hash = b};
+        }
     }
 
     [[nodiscard]] bool

fdf7ce8b05798643f8c29e007dfbb281847ba678
Vicky Chen <yifanc3@illinois.edu>
2024-01-18 21:57:47 +0000

    bump monad-trie

--- src/monad/execution/ethereum/replay_ethereum_ondisk_triedb.cpp
+++ src/monad/execution/ethereum/replay_ethereum_ondisk_triedb.cpp
@@ -15,12 +15,13 @@ int main(int argc, char *argv[])
     bool compaction = false;
     std::filesystem::path snapshot_file;
     std::vector<std::filesystem::path> deltas_files;
-    std::filesystem::path dbname_path = "replay_test.db";
+    std::vector<std::filesystem::path> dbname_paths;
     unsigned sq_thread_cpu = 15;
 
     CLI::App cli{"replay_ethereum_ondisk_triedb"};
     cli.add_flag("--append", append, "append at a specific block in db");
-    cli.add_option("--db-name", dbname_path, "db file name");
+    cli.add_option(
+        "--db-names", dbname_paths, "db file names, can have more than one");
     cli.add_option("--kcpu", sq_thread_cpu, "io_uring sq_thread_cpu");
     cli.add_option(
         "--snapshot-file", snapshot_file, "snapshot json file to load from");
@@ -28,6 +29,10 @@ int main(int argc, char *argv[])
         "--deltas-files", snapshot_file, "state delta json file to load from");
     cli.add_flag("--compaction", compaction, "do compaction");
     cli.parse(argc, argv);
+
+    if (dbname_paths.empty()) {
+        dbname_paths.emplace_back("test.db");
+    }
 
     std::unique_ptr<db::TrieDb> db;
     if (append) {
@@ -38,7 +43,7 @@ int main(int argc, char *argv[])
             .wr_buffers = 128,
             .uring_entries = 128,
             .sq_thread_cpu = sq_thread_cpu,
-            .dbname = dbname_path});
+            .dbname_paths = std::move(dbname_paths)});
     }
     else {
         MONAD_ASSERT(!snapshot_file.empty());
@@ -51,7 +56,7 @@ int main(int argc, char *argv[])
                 .wr_buffers = 128,
                 .uring_entries = 128,
                 .sq_thread_cpu = sq_thread_cpu,
-                .dbname = dbname_path},
+                .dbname_paths = std::move(dbname_paths)},
             input,
             false /* DO NOT insert code*/);
     }

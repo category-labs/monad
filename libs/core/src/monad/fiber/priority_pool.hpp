9397ae8a289ae9b36f10330ca572e97b754b9c12
Niall Douglas <niall@monad.xyz>
2024-06-27 19:22:15 +0100

    Revert "added wrappers.hpp and moved priority_pool to c impl." as it was causing CI to fail.
    This reverts commit 32b2be16dc7265739f9f73d04164b7629bb19119.

--- libs/core/src/monad/fiber/priority_pool.hpp
+++ libs/core/src/monad/fiber/priority_pool.hpp
@@ -3,7 +3,6 @@
 #include <monad/fiber/config.hpp>
 #include <monad/fiber/priority_queue.hpp>
 #include <monad/fiber/priority_task.hpp>
-#include <monad/fiber/wrappers.hpp>
 
 #include <boost/fiber/buffered_channel.hpp>
 #include <boost/fiber/condition_variable.hpp>
@@ -19,34 +18,18 @@ class PriorityPool final
 {
     PriorityQueue queue_{};
 
-    // bool done_{false};
+    bool done_{false};
 
     boost::fibers::mutex mutex_{};
     boost::fibers::condition_variable cv_{};
 
-    scheduler sched_;
     std::vector<std::thread> threads_{};
 
-    struct monad_fiber_task_deleter
-    {
-        void operator()(monad_fiber_task_t *t) const
-        {
-            t->destroy(t);
-        }
+    boost::fibers::buffered_channel<PriorityTask> channel_{1024};
 
-        void operator()(monad_fiber_t *t) const
-        {
-            (*this)(&t->task);
-        }
-    };
+    std::vector<boost::fibers::fiber> fibers_{};
 
-    channel<std::unique_ptr<monad_fiber_task_t, monad_fiber_task_deleter>>
-        channel_{1024};
-    std::vector<std::unique_ptr<monad_fiber_t, monad_fiber_task_deleter>>
-        fibers_;
-
-    void run_fiber_() noexcept;
-    std::atomic_size_t working_;
+    std::promise<void> start_{};
 
 public:
     PriorityPool(unsigned n_threads, unsigned n_fibers);
@@ -56,32 +39,9 @@ public:
 
     ~PriorityPool();
 
-    template <typename Func>
-    void submit(int64_t const priority, Func &&func)
+    void submit(uint64_t const priority, std::function<void()> task)
     {
-        struct func_t : monad_fiber_task_t
-        {
-            func_t(int64_t const priority, Func &&func)
-                : monad_fiber_task_t{
-                      +[](monad_fiber_task_t *task) {
-                          static_cast<struct func_t *>(task)->func();
-                      },
-                      +[](monad_fiber_task_t *task) {
-                          delete static_cast<struct func_t *>(task);
-                      },
-                      priority}
-                , func(std::forward<Func>(func))
-            {
-            }
-
-            std::decay_t<Func> func;
-        };
-
-        using pt =
-            std::unique_ptr<monad_fiber_task_t, monad_fiber_task_deleter>;
-        pt p{std::make_unique<func_t>(priority, std::forward<Func>(func))
-                 .release()};
-        channel_.send(std::move(p));
+        channel_.push({priority, std::move(task)});
     }
 };
 

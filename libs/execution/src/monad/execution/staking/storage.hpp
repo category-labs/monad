2b716e51b81a246a0340f1c74e4980beae2d864e
Kevin Kuehler <kkuehler@category.xyz>
2025-03-17 19:14:42 -0400

    staking: Add StorageType<> data structure
    A helper class that converts between a structured type and an array of
    bytes32. This is needed in staking to persist ValidatorInfo types into
    storage.

---
+++ libs/execution/src/monad/execution/staking/storage.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <monad/config.hpp>
+#include <monad/core/bytes.hpp>
+
+#include <type_traits>
+
+MONAD_NAMESPACE_BEGIN
+
+template <typename T>
+constexpr size_t num_storage_slots()
+{
+    constexpr size_t SLOT_SIZE = sizeof(bytes32_t);
+    return (sizeof(T) + SLOT_SIZE - 1) / SLOT_SIZE;
+}
+
+// This type abstracts storage any T type across the minimum number of EVM
+// storage slots required to store that type.
+template <typename T>
+    requires std::is_trivially_copyable_v<T>
+struct StorageAdapter
+{
+    static constexpr size_t N = num_storage_slots<T>();
+
+    union
+    {
+        struct
+        {
+            bytes32_t raw[N];
+
+            constexpr size_t size() const noexcept
+            {
+                return N;
+            }
+
+            constexpr bytes32_t &operator[](size_t const i) noexcept
+            {
+                return raw[i];
+            }
+
+            constexpr bytes32_t const &operator[](size_t const i) const noexcept
+            {
+                return raw[i];
+            }
+
+        } slots;
+
+        T typed;
+    };
+
+    StorageAdapter()
+        : typed{}
+    {
+    }
+};
+
+MONAD_NAMESPACE_END

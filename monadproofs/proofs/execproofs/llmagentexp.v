6d809841331a287157d5d66b2fa8c92edcf3f9c7
Abhishek Anand (Monad Labs) <abhishek.anand.iitg@gmail.com>
2025-05-20 09:57:33 -0400

    first run. my mistake. the correct c++ file was not imported

--- monadproofs/proofs/execproofs/llmagentexp.v
+++ monadproofs/proofs/execproofs/llmagentexp.v
@@ -2,7 +2,7 @@ Require Import bluerock.auto.invariants.
 Require Import bluerock.auto.cpp.proof.
 Require Import monad.proofs.misc.
 Require Import bluerock.auto.cpp.tactics4.
-Require Import monad.tutorials.demo.
+Require Import monad.tutorials.demo2.
 Require Import monad.tutorials.demomisc.
 From AAC_tactics Require Import AAC.
 From AAC_tactics Require Import Instances.
@@ -21,22 +21,40 @@ Disable Notation "`div`" (all).
 Disable Notation intR.
 Disable Notation uintR.
 Import linearity.
-
 Disable Notation atomic_name'.
+Disable Notation atomic_name.
 Open Scope pstring_scope.
 Set Printing FullyQualifiedNames.
 
-Print bluerock.lang.cpp.syntax.stmt.Stmt.
 
+Section with_Sigma.
+  Context `{Sigma:cpp_logic} {CU: genv}.
+  Context  {MODd : demo2.module ⊧ CU}.
+  Coercion cQpc : Qp >-> cQp.t.
 
-
+  Open Scope Z_scope.
 
 (*
-bluerock.lang.cpp.syntax.stmt.Stmt is an Inductive type I have defined for C++ statements.
-`bluerock.lang.cpp.syntax.stmt.Stmt` is defined mutually Inductively with many other types like `Expr`.
-Write a set of mutually recursive pretty-printer functions for all such types.
-These Gallina functions should return `PrimString.string`.
+Write a Rep predicate for the following C++ class and a spec for the add method.
+
+```c++
+class uint256 {
+public:
+  using limb_t = unsigned long long;          // assumes 64-bit limbs
+  limb_t data[4];                             // little-endian: data[0] is least-significant
+
+  uint256() : data{0, 0, 0, 0} {}
+
+  void add(const uint256& other);
+};
+```
+
++++ FILES
+../../fmai/prompts/sep.md
+../../fmai/prompts/specs.md
 
 +++ QUERIES
-Print bluerock.lang.cpp.syntax.stmt.Stmt.
-*)
+Check "unsigned long long"%cpp_type.
+
+ *)
+

c082f6b2b6e7686834893571895ce179a0d607e9
Bruce Collie <brucecollie82@gmail.com>
2025-07-10 07:24:27 -0400

    test

--- libs/execution/src/monad/core/address.hpp
+++ libs/execution/src/monad/core/address.hpp
@@ -16,6 +16,9 @@ static_assert(alignof(Address) == 1);
 
 MONAD_NAMESPACE_END
 
+// TODO : Remove this when we can use C++20's std::hash specialization.
+// dummy comment to avoid C++20's std::hash specialization warning
+// this isn't a real change, just a workaround for the warning
 namespace boost
 {
     inline size_t hash_value(monad::Address const &address) noexcept

f16872ee532bffbd377fc2e467921f19e53ab22a
Abhishek Anand (Monad Labs) <abhishek.anand.iitg@gmail.com>
2025-10-03 17:48:25 -0400

    renamings: State::get_balance --> State::get_current_balance_pessimistic, State::get_original_balance --> State::get_original_balance_pessimistic

--- category/execution/ethereum/execute_transaction_test.cpp
+++ category/execution/ethereum/execute_transaction_test.cpp
@@ -110,7 +110,8 @@ TEST(TransactionProcessor, irrevocable_gas_and_refund_new_contract)
     {
         State state{bs, Incarnation{0, 0}};
         EXPECT_EQ(
-            intx::be::load<uint256_t>(state.get_balance(from)),
+            intx::be::load<uint256_t>(
+                state.get_current_balance_pessimistic(from)),
             uint256_t{55'999'999'999'470'000});
         EXPECT_EQ(state.get_nonce(from), 26); // EVMC will inc for creation
     }
@@ -268,7 +269,8 @@ TEST(TransactionProcessor, refunds_delete)
         {
             State state{bs, Incarnation{0, 0}};
             EXPECT_EQ(
-                intx::be::load<uint256_t>(state.get_balance(from)),
+                intx::be::load<uint256_t>(
+                    state.get_current_balance_pessimistic(from)),
                 uint256_t{40'296'100'000'000'000});
         }
     }
@@ -316,7 +318,8 @@ TEST(TransactionProcessor, refunds_delete)
         {
             State state{bs, Incarnation{0, 0}};
             EXPECT_EQ(
-                intx::be::load<uint256_t>(state.get_balance(from)),
+                intx::be::load<uint256_t>(
+                    state.get_current_balance_pessimistic(from)),
                 uint256_t{
                     37'093'800'000'000'000 + (120'000 * 100'000'000'000)});
         }
@@ -404,7 +407,8 @@ TEST(TransactionProcessor, refunds_delete_then_set)
         {
             State state{bs, Incarnation{0, 0}};
             EXPECT_EQ(
-                intx::be::load<uint256_t>(state.get_balance(from)),
+                intx::be::load<uint256_t>(
+                    state.get_current_balance_pessimistic(from)),
                 uint256_t{52'559'100'000'000'000 + (2800 * 100'000'000'000)});
         }
     }

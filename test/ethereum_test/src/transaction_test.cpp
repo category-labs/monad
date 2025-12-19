fefcd645c376df4f442e2d5ae1d7d2cd03124949
goodlyrottenapple <goodlyrottenapple@gmail.com>
2025-12-19 06:16:18 -0500

    ethereum_test improvements

--- test/ethereum_test/src/transaction_test.cpp
+++ test/ethereum_test/src/transaction_test.cpp
@@ -13,8 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <ethereum_test.hpp>
 #include <from_json.hpp>
+#include <revision_map.hpp>
 #include <transaction_test.hpp>
 
 #include <category/core/assert.h>
@@ -51,39 +51,6 @@
 
 MONAD_ANONYMOUS_NAMESPACE_BEGIN
 
-void register_tests(
-    std::filesystem::path const &root,
-    std::optional<evmc_revision> const &revision)
-{
-    namespace fs = std::filesystem;
-    MONAD_ASSERT(fs::exists(root) && fs::is_directory(root));
-
-    for (auto const &entry : fs::recursive_directory_iterator{root}) {
-        auto const path = entry.path();
-        if (path.extension() == ".json") {
-            MONAD_ASSERT(entry.is_regular_file());
-
-            auto test = fmt::format("{}", fs::relative(path, root).string());
-            std::ranges::replace(test, '-', '_');
-
-            testing::RegisterTest(
-                "TransactionTests",
-                test.c_str(),
-                nullptr,
-                nullptr,
-                path.string().c_str(),
-                0,
-                [=] -> testing::Test * {
-                    return new test::TransactionTest(path, revision);
-                });
-        }
-    }
-}
-
-MONAD_ANONYMOUS_NAMESPACE_END
-
-MONAD_TEST_NAMESPACE_BEGIN
-
 template <Traits traits>
 void process_transaction(Transaction const &txn, nlohmann::json const &expected)
 {
@@ -113,13 +80,23 @@ void process_transaction(Transaction const &txn, nlohmann::json const &expected)
 }
 
 void process_transaction(
-    evmc_revision const rev, Transaction const &txn,
-    nlohmann::json const &expected)
+    std::variant<evmc_revision, monad_revision> const revision,
+    Transaction const &txn, nlohmann::json const &expected)
 {
-    MONAD_ASSERT(rev != EVMC_CONSTANTINOPLE);
-    SWITCH_EVM_TRAITS(process_transaction, txn, expected);
-    MONAD_ASSERT(false);
+    if (std::holds_alternative<evmc_revision>(revision)) {
+        auto const rev = std::get<evmc_revision>(revision);
+        MONAD_ASSERT(rev != EVMC_CONSTANTINOPLE);
+        SWITCH_EVM_TRAITS(process_transaction, txn, expected);
+    }
+    else {
+        auto const rev = std::get<monad_revision>(revision);
+        SWITCH_MONAD_TRAITS(process_transaction, txn, expected);
+    }
 }
+
+MONAD_ANONYMOUS_NAMESPACE_END
+
+MONAD_TEST_NAMESPACE_BEGIN
 
 void TransactionTest::TestBody()
 {
@@ -166,18 +143,50 @@ void TransactionTest::TestBody()
         process_transaction(rev, txn.value(), expected);
     }
 
-    if (!executed) {
-        MONAD_ASSERT(revision_.has_value());
-        GTEST_SKIP() << "no test cases found for revision="
-                     << revision_.value();
+    if (!executed && revision_.has_value()) {
+        std::visit(
+            [](auto const &r) {
+                GTEST_SKIP() << "no test cases found for revision=" << r;
+            },
+            revision_.value());
     }
 }
 
-void register_transaction_tests(std::optional<evmc_revision> const &revision)
+void register_transaction_tests_path(
+    std::filesystem::path const &root,
+    std::optional<std::variant<evmc_revision, monad_revision>> const &revision)
 {
-    register_tests(
+    namespace fs = std::filesystem;
+    MONAD_ASSERT(fs::exists(root) && fs::is_directory(root));
+
+    for (auto const &entry : fs::recursive_directory_iterator{root}) {
+        auto const path = entry.path();
+        if (path.extension() == ".json") {
+            MONAD_ASSERT(entry.is_regular_file());
+
+            auto test = fmt::format("{}", fs::relative(path, root).string());
+            std::ranges::replace(test, '-', '_');
+
+            testing::RegisterTest(
+                "TransactionTests",
+                test.c_str(),
+                nullptr,
+                nullptr,
+                path.string().c_str(),
+                0,
+                [=] -> testing::Test * {
+                    return new test::TransactionTest(path, revision);
+                });
+        }
+    }
+}
+
+void register_transaction_tests(
+    std::optional<std::variant<evmc_revision, monad_revision>> const &revision)
+{
+    register_transaction_tests_path(
         test_resource::ethereum_tests_dir / "TransactionTests", revision);
-    register_tests(
+    register_transaction_tests_path(
         test_resource::build_dir /
             "src/ExecutionSpecTestFixtures/transaction_tests",
         revision);

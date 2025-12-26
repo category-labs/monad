7b5a6b83320872520a6f53b54e935fb67398c866
Andreas Lynge <12626795+andreaslyn@users.noreply.github.com>
2025-12-26 18:43:20 +0100

    VM memory pool

--- test/vm/execution_benchmarks/benchmarks.cpp
+++ test/vm/execution_benchmarks/benchmarks.cpp
@@ -59,6 +59,8 @@ using enum BlockchainTestVM::Implementation;
 
 using namespace monad::test;
 
+using monad::vm::test::TestMemory;
+
 struct free_message
 {
     static void operator()(evmc_message *msg) noexcept
@@ -86,8 +88,8 @@ namespace
                               "VMTests" / "vmPerformance";
 
     auto make_benchmark(
-        std::string const &name, std::span<std::uint8_t const> code,
-        std::span<std::uint8_t const> input)
+        TestMemory &test_memory, std::string const &name,
+        std::span<std::uint8_t const> code, std::span<std::uint8_t const> input)
     {
         auto *code_buffer = new std::uint8_t[code.size()];
         std::copy(code.begin(), code.end(), code_buffer);
@@ -109,9 +111,9 @@ namespace
             .code_address = {},
             .code = code_buffer,
             .code_size = code.size(),
-            .memory_handle = nullptr,
-            .memory = nullptr,
-            .memory_capacity = 0,
+            .memory_handle = test_memory.data,
+            .memory = test_memory.data,
+            .memory_capacity = TestMemory::capacity,
         });
 
         return benchmark_case{name, std::move(msg)};
@@ -125,7 +127,7 @@ namespace
             std::istreambuf_iterator<char>{}};
     }
 
-    auto load_benchmark(fs::path const &path)
+    auto load_benchmark(TestMemory &test_memory, fs::path const &path)
     {
         MONAD_VM_DEBUG_ASSERT(fs::is_directory(path));
 
@@ -136,6 +138,7 @@ namespace
         MONAD_VM_DEBUG_ASSERT(fs::is_regular_file(calldata_path));
 
         return make_benchmark(
+            test_memory,
             path.stem().string(),
             read_file(contract_path),
             read_file(calldata_path));
@@ -249,13 +252,13 @@ namespace
         }
     }
 
-    auto benchmarks() noexcept
+    auto benchmarks(TestMemory &test_memory) noexcept
     {
         auto ret = std::vector<benchmark_case>{};
 
         for (auto const &p :
              fs::directory_iterator(execution_benchmarks_dir / "basic")) {
-            ret.emplace_back(load_benchmark(p));
+            ret.emplace_back(load_benchmark(test_memory, p));
         }
 
         return ret;
@@ -275,7 +278,8 @@ namespace
         return ret;
     }
 
-    void register_benchmark_json(std::vector<BenchmarkTest> const &tests)
+    void register_benchmark_json(
+        std::vector<BenchmarkTest> const &tests, TestMemory &test_memory)
     {
 
         for (auto const &test : tests) {
@@ -302,9 +306,9 @@ namespace
                         .code_address = recipient,
                         .code = nullptr,
                         .code_size = 0,
-                        .memory_handle = nullptr,
-                        .memory = nullptr,
-                        .memory_capacity = 0,
+                        .memory_handle = test_memory.data,
+                        .memory = test_memory.data,
+                        .memory_capacity = TestMemory::capacity,
                     };
 
                     std::vector<std::string> const failure_tests = {
@@ -338,7 +342,8 @@ namespace
 
 int main(int argc, char **argv)
 {
-    auto const all_bms = benchmarks();
+    TestMemory test_memory;
+    auto const all_bms = benchmarks(test_memory);
 
     for (auto const &bm : all_bms) {
         register_benchmark(bm.name, *bm.msg);
@@ -347,7 +352,7 @@ int main(int argc, char **argv)
     auto const all_bms_json = benchmarks_json();
 
     for (auto const &path : all_bms_json) {
-        register_benchmark_json(path);
+        register_benchmark_json(path, test_memory);
     }
 
     benchmark::Initialize(&argc, argv);

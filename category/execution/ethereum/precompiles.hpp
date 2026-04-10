314ed914346714f31c27296026820a097b13a6a1
Daniel Hillerström <1827113+dhil@users.noreply.github.com>
2026-04-10 09:59:02 -0400

    blake2bf precompile
    Co-authored-by: mkolosick <mkolosick@category.xyz>

--- category/execution/ethereum/precompiles.hpp
+++ category/execution/ethereum/precompiles.hpp
@@ -145,6 +145,7 @@ PrecompileResult expmod_execute(byte_string_view);
 PrecompileResult ecadd_execute(byte_string_view);
 PrecompileResult ecmul_execute(byte_string_view);
 PrecompileResult snarkv_execute(byte_string_view);
+PrecompileResult blake2bf_execute(byte_string_view);
 
 struct PrecompileImplResult
 {
@@ -182,5 +183,8 @@ ecmul_impl(byte_string_view input, std::span<uint8_t, 64> const out);
 
 PrecompileImplResult
 snarkv_impl(byte_string_view input, std::span<uint8_t, 32> const out);
+
+PrecompileImplResult
+blake2bf_impl(byte_string_view input, std::span<uint8_t, 64> const out);
 
 MONAD_NAMESPACE_END

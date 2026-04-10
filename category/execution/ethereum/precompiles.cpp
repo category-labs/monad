b4a003912324ae4e91dd69850ff3312593deb4f4
Daniel Hillerström <1827113+dhil@users.noreply.github.com>
2026-04-10 09:55:29 -0400

    snarkv precompile
    Co-authored-by: mkolosick <mkolosick@category.xyz>

--- category/execution/ethereum/precompiles.cpp
+++ category/execution/ethereum/precompiles.cpp
@@ -324,4 +324,17 @@ PrecompileResult ecmul_execute(byte_string_view const input)
         ecmul_impl(clamped_input, std::span<uint8_t, 64>{out, 64}), out);
 }
 
+PrecompileResult snarkv_execute(byte_string_view const input)
+{
+    if (input.size() % 192 != 0) {
+        return PrecompileResult::failure();
+    }
+
+    auto *const out = static_cast<uint8_t *>(std::malloc(32));
+    MONAD_ASSERT(out != nullptr);
+    return from_impl_result(
+        snarkv_impl(input, std::span<uint8_t, 32>{out, 32}), out);
+}
+
+
 MONAD_NAMESPACE_END

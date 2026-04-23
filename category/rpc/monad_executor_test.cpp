4c73c5ea0a5f246ad4ad81d24c361c0411a6600b
Kevin Kuehler <kkuehler@category.xyz>
2026-04-23 14:48:53 -0400

    [execution]: Automatically distribute priority fees to stakers
    The staking contract already keeps state of the current proposer. This
    was the larger piece of work, and this PR strings together existing
    functionality in the staking contract.
    
    At the end of each transaction, instead of transferring fees to the
    beneficary, is collected in a special staging address. MIP-11
    refers to this as the "distribution account", which is the constant
    `PRIORITY_FEE_DIST_ADDRESS` introduced in this PR.
    
    At the end of each block, the balance in distribution account balance
    is transferred to the block proposers validator pool using an internal
    version of the externalRewards() precompile. If the fees are less than
    the dust, no balance is transferred.
    
    https://github.com/monad-crypto/MIPs/blob/main/MIPS/MIP-11.md

--- category/rpc/monad_executor_test.cpp
+++ category/rpc/monad_executor_test.cpp
@@ -1931,6 +1931,9 @@ TEST_F(EthCallFixture, trace_block_with_prestate)
                     "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa": {
                         "balance": "0x1",
                         "nonce": 1
+                    },
+                    "0xfee5fee5fee5fee5fee5fee5fee5fee5fee5fee5": {
+                        "balance": "0x0"
                     }
                 },
                 "txHash": "0x659b50d5b5c543fb681ace210eb175938d5b829297bcf579bc186ac0ea0874dd"
@@ -1944,6 +1947,9 @@ TEST_F(EthCallFixture, trace_block_with_prestate)
                     "0xc48513273d60b70ee2f25d5c4256612a91573e1e": {
                         "balance": "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
                         "nonce": 2
+                    },
+                    "0xfee5fee5fee5fee5fee5fee5fee5fee5fee5fee5": {
+                        "balance": "0x0"
                     }
                 },
                 "txHash": "0x0b391cd83e5ebd2a5d5c0223c4258eaa364737cc6cb9f88c83eced5eb2543e8b"
@@ -2169,6 +2175,9 @@ TEST_F(EthCallFixture, trace_transaction_with_prestate)
             "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa": {
                 "balance": "0x1",
                 "nonce": 1
+            },
+            "0xfee5fee5fee5fee5fee5fee5fee5fee5fee5fee5": {
+                "balance": "0x0"
             }
         })";
 
@@ -2211,6 +2220,9 @@ TEST_F(EthCallFixture, trace_transaction_with_prestate)
                 "balance":
                 "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
                 "nonce": 2
+            },
+            "0xfee5fee5fee5fee5fee5fee5fee5fee5fee5fee5": {
+                "balance": "0x0"
             }
         })";
         EXPECT_EQ(
@@ -2719,6 +2731,9 @@ TEST_F(EthCallFixture, prestate_trace_near_genesis)
             "0xbedcab535cc48a9bd659ac448f092d02ba23a05a": {
                 "balance": "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
                 "nonce":1
+            },
+            "0xfee5fee5fee5fee5fee5fee5fee5fee5fee5fee5": {
+                "balance": "0x0"
             }
         })";
         EXPECT_EQ(
@@ -2782,6 +2797,9 @@ TEST_F(EthCallFixture, prestate_trace_near_genesis)
             "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa": {
                 "balance": "0x1",
                 "nonce":1
+            },
+            "0xfee5fee5fee5fee5fee5fee5fee5fee5fee5fee5": {
+                "balance": "0x0"
             }
         })";
 
@@ -4174,9 +4192,8 @@ TEST_F(EthCallFixture, trace_transaction_with_rewards_prestate)
                 "balance": "0x1",
                 "nonce": 1
             },
-            "0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb": {
-                "balance": "0x0",
-                "nonce": 1
+            "0xfee5fee5fee5fee5fee5fee5fee5fee5fee5fee5": {
+                "balance": "0x0"
             }
         })";
 
@@ -4215,14 +4232,13 @@ TEST_F(EthCallFixture, trace_transaction_with_rewards_prestate)
                 "balance": "0xabbb",
                 "nonce": 1
             },
-            "0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb": {
-                "balance": "0x1b7740",
-                "nonce": 1
-            },
             "0xea4f2322426da1c68b6b7b8cc244c4fa0f14b7d3": {
                 "balance":
                 "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
                 "nonce": 2
+            },
+            "0xfee5fee5fee5fee5fee5fee5fee5fee5fee5fee5": {
+                "balance": "0x1b7740"
             }
         })";
         EXPECT_EQ(
@@ -4268,7 +4284,7 @@ TEST_F(EthCallFixture, trace_transaction_with_rewards_prestate)
                 "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa": {
                     "balance": "0xabbb"
                 },
-                "0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb": {
+                "0xfee5fee5fee5fee5fee5fee5fee5fee5fee5fee5": {
                     "balance": "0x1b7740"
                 }
             },
@@ -4280,10 +4296,6 @@ TEST_F(EthCallFixture, trace_transaction_with_rewards_prestate)
                 },
                 "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa": {
                     "balance": "0x1",
-                    "nonce": 1
-                },
-                "0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb": {
-                    "balance": "0x0",
                     "nonce": 1
                 }
             }
@@ -4324,13 +4336,13 @@ TEST_F(EthCallFixture, trace_transaction_with_rewards_prestate)
                 "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa": {
                     "balance": "0x15775"
                 },
-                "0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb": {
-                    "balance": "0x36ee80"
-                },
                 "0xea4f2322426da1c68b6b7b8cc244c4fa0f14b7d3": {
                     "balance":
                     "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe0cfc5",
                     "nonce": 3
+                },
+                "0xfee5fee5fee5fee5fee5fee5fee5fee5fee5fee5": {
+                    "balance": "0x36ee80"
                 }
             },
             "pre": {
@@ -4338,14 +4350,13 @@ TEST_F(EthCallFixture, trace_transaction_with_rewards_prestate)
                     "balance": "0xabbb",
                     "nonce": 1
                 },
-                "0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb": {
-                    "balance": "0x1b7740",
-                    "nonce": 1
-                },
                 "0xea4f2322426da1c68b6b7b8cc244c4fa0f14b7d3": {
                     "balance":
                     "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
                     "nonce": 2
+                },
+                "0xfee5fee5fee5fee5fee5fee5fee5fee5fee5fee5": {
+                    "balance": "0x1b7740"
                 }
             }
         })";
--- category/vm/evm/traits.hpp
+++ category/vm/evm/traits.hpp
@@ -56,6 +56,7 @@ namespace monad
         { T::eip_7883_active() } -> std::same_as<bool>;
         { T::eip_7951_active() } -> std::same_as<bool>;
         { T::mip_3_active() } -> std::same_as<bool>;
+        { T::mip_11_active() } -> std::same_as<bool>;
         { T::can_create_inside_delegated() } -> std::same_as<bool>;
 
         // Constants
@@ -118,6 +119,11 @@ namespace monad
         }
 
         static consteval bool mip_3_active() noexcept
+        {
+            return false;
+        }
+
+        static consteval bool mip_11_active() noexcept
         {
             return false;
         }
@@ -229,6 +235,11 @@ namespace monad
                 return true;
             }
             return false;
+        }
+
+        static consteval bool mip_11_active() noexcept
+        {
+            return Rev >= MONAD_NEXT;
         }
 
         static consteval bool can_create_inside_delegated() noexcept

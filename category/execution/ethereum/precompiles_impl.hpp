ee4b0b50375f03d52c1fc2a64f8299a88eebf8fa
mkolosick <mkolosick@category.xyz>
2026-05-27 14:03:56 -0400

    Build zkvm native static library
    Add zkvm_accelerators and shim to call out to them in
    zkvm/category/execution/ethereum/precompiles_impl.hpp.

--- category/execution/ethereum/precompiles_impl.hpp
+++ category/execution/ethereum/precompiles_impl.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Category Labs, Inc.
+// Copyright (C) 2025-26 Category Labs, Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,6 +12,8 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
 
 #include <category/core/assert.h>
 #include <category/core/byte_string.hpp>
@@ -44,8 +46,7 @@
 #include <setup/settings.h>
 #include <setup/setup.h>
 
-#include <stdio.h>
-
+#include <silkpre/ecdsa.h>
 #include <silkpre/precompile.h>
 
 #include <bit>
@@ -114,18 +115,20 @@ static inline PrecompileResult silkpre_execute(byte_string_view const input)
     auto const [output, output_size] = Func(input.data(), input.size());
     if (output == nullptr) {
         MONAD_ASSERT(output_size == 0);
-        return {EVMC_PRECOMPILE_FAILURE, nullptr, 0};
+        return PrecompileResult::failure();
     }
     return {EVMC_SUCCESS, output, output_size};
 }
 
-PrecompileResult ecrecover_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+ecrecover_execute(byte_string_view const input)
 {
     auto const clamped_input = input.substr(0, 128);
     return silkpre_execute<silkpre_ecrec_run>(clamped_input);
 }
 
-PrecompileResult sha256_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+sha256_execute(byte_string_view const input)
 {
     auto *const output = static_cast<uint8_t *>(std::malloc(32));
     MONAD_ASSERT(output != nullptr);
@@ -139,7 +142,8 @@ PrecompileResult sha256_execute(byte_string_view const input)
     return {EVMC_SUCCESS, output, 32};
 }
 
-PrecompileResult ripemd160_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+ripemd160_execute(byte_string_view const input)
 {
     auto *const output = static_cast<uint8_t *>(std::malloc(32));
     MONAD_ASSERT(output != nullptr);
@@ -152,19 +156,22 @@ PrecompileResult ripemd160_execute(byte_string_view const input)
     return {EVMC_SUCCESS, output, 32};
 }
 
-PrecompileResult ecadd_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+ecadd_execute(byte_string_view const input)
 {
     auto const clamped_input = input.substr(0, 128);
     return silkpre_execute<silkpre_bn_add_run>(clamped_input);
 }
 
-PrecompileResult ecmul_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+ecmul_execute(byte_string_view const input)
 {
     auto const clamped_input = input.substr(0, 96);
     return silkpre_execute<silkpre_bn_mul_run>(clamped_input);
 }
 
-PrecompileResult identity_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+identity_execute(byte_string_view const input)
 {
     if (input.empty()) {
         return {EVMC_SUCCESS, nullptr, 0};
@@ -176,17 +183,20 @@ PrecompileResult identity_execute(byte_string_view const input)
     return {EVMC_SUCCESS, output, input.size()};
 }
 
-PrecompileResult expmod_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+expmod_execute(byte_string_view const input)
 {
     return silkpre_execute<silkpre_expmod_run>(input);
 }
 
-PrecompileResult snarkv_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+snarkv_execute(byte_string_view const input)
 {
     return silkpre_execute<silkpre_snarkv_run>(input);
 }
 
-PrecompileResult blake2bf_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+blake2bf_execute(byte_string_view const input)
 {
     if (input.size() != 213) {
         return {EVMC_PRECOMPILE_FAILURE, nullptr, 0};
@@ -221,7 +231,8 @@ PrecompileResult blake2bf_execute(byte_string_view const input)
     return {EVMC_SUCCESS, output, 64};
 }
 
-PrecompileResult point_evaluation_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+point_evaluation_execute(byte_string_view const input)
 {
     if (input.size() != 192) {
         return PrecompileResult::failure();
@@ -262,37 +273,44 @@ PrecompileResult point_evaluation_execute(byte_string_view const input)
     };
 }
 
-PrecompileResult bls12_g1_add_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+bls12_g1_add_execute(byte_string_view const input)
 {
     return bls12::add<bls12::G1>(input);
 }
 
-PrecompileResult bls12_g1_msm_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+bls12_g1_msm_execute(byte_string_view const input)
 {
     return bls12::msm<bls12::G1>(input);
 }
 
-PrecompileResult bls12_g2_add_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+bls12_g2_add_execute(byte_string_view const input)
 {
     return bls12::add<bls12::G2>(input);
 }
 
-PrecompileResult bls12_g2_msm_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+bls12_g2_msm_execute(byte_string_view const input)
 {
     return bls12::msm<bls12::G2>(input);
 }
 
-PrecompileResult bls12_pairing_check_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+bls12_pairing_check_execute(byte_string_view const input)
 {
     return bls12::pairing_check(input);
 }
 
-PrecompileResult bls12_map_fp_to_g1_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+bls12_map_fp_to_g1_execute(byte_string_view const input)
 {
     return bls12::map_fp_to_g<bls12::G1>(input);
 }
 
-PrecompileResult bls12_map_fp2_to_g2_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+bls12_map_fp2_to_g2_execute(byte_string_view const input)
 {
     return bls12::map_fp_to_g<bls12::G2>(input);
 }
@@ -300,7 +318,8 @@ PrecompileResult bls12_map_fp2_to_g2_execute(byte_string_view const input)
 // Rollup precompiles
 
 // EIP-7951
-PrecompileResult p256_verify_execute(byte_string_view const input)
+[[gnu::always_inline]] inline PrecompileResult
+p256_verify_execute(byte_string_view const input)
 {
     using namespace CryptoPP;
 

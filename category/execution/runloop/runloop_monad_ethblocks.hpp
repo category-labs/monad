5df73f3b6268bae38e357e1f73850eafe54a56a5
Vicky Chen <vchen@category.xyz>
2026-08-13 12:28:20 -0400

    [runloop] allow for dual-db commit in runloop_monad_ethblocks where secondary_db is optional

--- category/execution/runloop/runloop_monad_ethblocks.hpp
+++ category/execution/runloop/runloop_monad_ethblocks.hpp
@@ -43,6 +43,7 @@ Result<std::pair<uint64_t, uint64_t>> runloop_monad_ethblocks(
     MonadChain const &, std::filesystem::path const &, Db &, vm::VM &,
     BlockHashBufferFinalized &, fiber::PriorityPool &, uint64_t &, uint64_t,
     sig_atomic_t const volatile &, bool enable_tracing,
-    std::chrono::seconds block_db_timeout, ExecutionEventRecorder *);
+    std::chrono::seconds block_db_timeout, ExecutionEventRecorder *,
+    Db *secondary_db);
 
 MONAD_NAMESPACE_END

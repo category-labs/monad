a52ebee8356c3e2c47d347e565f7d77a6b3e47a8
goodlyrottenapple <goodlyrottenapple@gmail.com>
2026-08-14 04:51:01 -0400

    switch all but core/runtime calls to std::byteswap to bswap; shadow std::byteswap in zkvm

--- category/execution/ethereum/precompiles.hpp
+++ category/execution/ethereum/precompiles.hpp
@@ -18,6 +18,9 @@
 #include <category/core/address.hpp>
 #include <category/core/byte_string.hpp>
 #include <category/core/config.hpp>
+#include <category/core/int.hpp>
+#include <category/execution/ethereum/state3/state.hpp>
+#include <category/execution/ethereum/trace/call_tracer.hpp>
 #include <category/vm/evm/traits.hpp>
 
 #include <evmc/evmc.h>
@@ -94,7 +97,7 @@ blake2bf_gas_cost_ethereum(byte_string_view const input)
     static_assert(
         std::endian::native == std::endian::little,
         "blake2bf_gas_cost_ethereum only works on little-endian platforms");
-    return std::byteswap(rounds);
+    return bswap(rounds);
 }
 
 template <Traits traits>
